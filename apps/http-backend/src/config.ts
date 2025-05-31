68fa59d5d4e36356e977e26c58fea25262f55a37
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-05-31 22:04:59 +0530

    Added signup endpoints and db calls

---
+++ apps/http-backend/src/config.ts
@@ -0,0 +1,1 @@
+export const JWt_PASSWORD = process.env.JWT_PASSWORD || "1234random" 
--- apps/http-backend/src/routes/v1/user.ts
+++ apps/http-backend/src/routes/v1/user.ts
@@ -1,17 +1,59 @@
 import express, { Router } from "express";
-import authenticator from "authenticator";
+import authenticator, { verifyToken } from "authenticator";
+import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWt_PASSWORD } from "../../config";
 
 const router: Router = express.Router();
 
 router.post('/signup', (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
+    const number = req.body.phoneNumber;
 
-    const totp = authenticator.generateToken(phoneNumber + "SignUP");
+    const totp = authenticator.generateToken(number + "SignUP");
+
+    const user = client.user.upsert({
+        where: {
+            number
+        },
+        create: {
+            number,
+            name: ""
+        },
+        update: {}
+    })
+
     res.json({ "otp": totp, "id": 2 });
 });
 
 router.post('/signup/verify', (req, res) => {
-    res.json({});
+    const number = req.body.phoneNumber;
+    const name = req.body.name;
+
+    if (!verifyToken(number + "SignUP", req.body.otp)) {
+        res.status(401).json({
+            "message": "Invalid Token"
+        })
+
+        return;
+    }
+
+    const userId = client.user.update({
+        where: {
+            number
+        },
+        data: {
+            name,
+            verified: true
+        }
+    })
+
+    const jwtToken = jwt.sign({
+        userId
+    }, JWt_PASSWORD);
+
+    res.json({
+        jwtToken
+    });
 });
 
 export default router;
